8d2b5070d71cdc61fd986f556c62e292bdd10349
JenR <112354033+jrennr@users.noreply.github.com>
2023-08-06 18:47:10 -0700

    challenge_logic

---
+++ Earthquake_Challenge/Static/JS/challenge_logic.js
@@ -0,0 +1,99 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY // Use API_KEY instead of config.js
+});
+
+// We create the second tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY // Use API_KEY instead of config.js
+});
+
+// Create a third map tile layer using Mapbox styles.
+let lightMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  maxZoom: 18,
+  accessToken: API_KEY // Use API_KEY instead of config.js
+});
+
+// Create the map object with center, zoom level, and default layer.
+let map = L.map('mapid', {
+  center: [40.7, -94.5],
+  zoom: 3,
+  layers: [streets]
+});
+
+// Create a base layer that holds all three maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets,
+  "Light Map": lightMap // Add the third map to the baseMaps
+};
+
+// 1. Add a 2nd layer group for the tectonic plate data.
+let allEarthquakes = new L.LayerGroup();
+let tectonicPlates = new L.LayerGroup(); // Add a third layer group for the tectonic plate data.
+let majorEarthquakes = new L.LayerGroup(); // Add a fourth layer group for major earthquake data.
+
+// 2. Add a reference to the tectonic plates group to the overlays object.
+let overlays = {
+  "Earthquakes": allEarthquakes,
+  "Tectonic Plates": tectonicPlates, // Add the tectonic plate layer group to the overlays.
+  "Major Earthquakes": majorEarthquakes // Add the major earthquake layer group to the overlays
+};
+
+// Then we add a control to the map that will allow the user to change which
+// layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // ... (Rest of the code for earthquake data)
+
+  // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(plateData) {
+    // Create a GeoJSON layer with the retrieved tectonic plate data.
+    L.geoJson(plateData, {
+      style: {
+        color: "#ff6500", // Add color to the tectonic plate lines
+        weight: 2 // Add width to the tectonic plate lines
+      }
+    }).addTo(tectonicPlates); // Add the tectonic plate layer to the tectonicPlates layer group
+  });
+
+  // Here we create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
+
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // Loop through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  // Finally, we add our legend to the map.
+  legend.addTo(map);
+
+});
