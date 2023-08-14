549a1c3a7252cafa4ae00ec35f93eda9e723a1bf
JenR <112354033+jrennr@users.noreply.github.com>
2023-08-13 22:25:36 -0700

    Update challenge_logic.js

--- Earthquake_Challenge/Static/JS/challenge_logic.js
+++ Earthquake_Challenge/Static/JS/challenge_logic.js
@@ -5,20 +5,20 @@ console.log("working");
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  accessToken: API_KEY 
+  accessToken: config.js
 });
 
 // We create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  accessToken: API_KEY // Use API_KEY instead of config.js
+  accessToken: config.js
 });
 
 // Create a third map tile layer using Mapbox styles.
 let lightMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   maxZoom: 18,
-  accessToken: API_KEY 
+  accessToken: config.js
 });
 
 // Create the map object with center, zoom level, and default layer.
